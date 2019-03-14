00e23d6a567120eb73a4fbd12016320dfa9d6a7c
Anastasia Zabela <zabela113@mail.ru>
2019-03-14 23:54:42 +0300

    feat: add function of check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+	for (let i=0; i<bracketsConfig.length; i++) {
+		let property = bracketsConfig[i][0];
+		brackets[property] = bracketsConfig[i][1];;
+  }	
+  
+  function wrapStrArray (str) {
+    let array = [];
+    for (let i=0; i<str.length; i++) {
+      array.push(str[i]);
+    }
+    return array;
+  }
+
+	function checkBrackets (strArray) {
+		for (let i=0; i<strArray.length; i++) {
+			let a = strArray[i];
+			if (strArray[i+1] == brackets[a] && strArray[i+1] != undefined) {
+				strArray.splice(i, 2);
+				i=-1;
+			}
+		}
+		if (strArray[0]) {
+			return	false;
+		} else {
+			return	true;
+		}
+  }
+
+  let strArray = wrapStrArray(str);
+  let result = checkBrackets(strArray);
+  return result;
 }
